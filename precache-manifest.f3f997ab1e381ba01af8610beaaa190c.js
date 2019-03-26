337edc2ed1eb32bd6083c593fcde7522ee2aed08
sandeepsajan0 <sandeepsajan0@gmail.com>
2019-03-27 02:39:27 +0530

    Updates

---
+++ precache-manifest.f3f997ab1e381ba01af8610beaaa190c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "435f4bb6cd0bc258d1e6",
+    "url": "/ReactSwapi_api/static/css/main.c9b25542.chunk.css"
+  },
+  {
+    "revision": "435f4bb6cd0bc258d1e6",
+    "url": "/ReactSwapi_api/static/js/main.435f4bb6.chunk.js"
+  },
+  {
+    "revision": "16d8f92e87025c89655a",
+    "url": "/ReactSwapi_api/static/js/runtime~main.16d8f92e.js"
+  },
+  {
+    "revision": "a2bed75e91c7c9b67e12",
+    "url": "/ReactSwapi_api/static/js/2.a2bed75e.chunk.js"
+  },
+  {
+    "revision": "03ae819ca5491d6e53c1ff79cdbf3ed6",
+    "url": "/ReactSwapi_api/index.html"
+  }
+];
